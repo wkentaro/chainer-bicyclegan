673b007f296ef44dbae489870a3788e00111d014
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-12-30 06:01:21 +0900

    Update examples/pytorch2chainer/download_models.sh

--- examples/pytorch2chainer/download_models.sh
+++ examples/pytorch2chainer/download_models.sh
@@ -2,16 +2,28 @@
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-FILE=edges2shoes
-
-OUTPUT=$HERE/data/${FILE}_net_G.pth
-URL=https://people.eecs.berkeley.edu/~junyanz/projects/BicycleGAN/models/${FILE}_net_G.pth
+OUTPUT=$HERE/data/edges2shoes_net_E.pth
+URL=https://people.eecs.berkeley.edu/~junyanz/projects/BicycleGAN/models/edges2shoes_net_E.pth
 if [ ! -e $OUTPUT ]; then
   wget $URL -O $OUTPUT
 fi
 
-OUTPUT=$HERE/data/${FILE}_net_E.pth
-URL=https://people.eecs.berkeley.edu/~junyanz/projects/BicycleGAN/models/${FILE}_net_E.pth
+OUTPUT=$HERE/data/edges2shoes_net_G.pth
+URL=https://people.eecs.berkeley.edu/~junyanz/projects/BicycleGAN/models/edges2shoes_net_G.pth
 if [ ! -e $OUTPUT ]; then
   wget $URL -O $OUTPUT
+fi
+
+OUTPUT=$HERE/data/edges2shoes_net_E_from_chainer.npz
+URL=https://drive.google.com/uc?id=1Qr_F3HkaTipqqpBBkuxJa3vLJea6bJ2C
+if [ ! -e $OUTPUT ]; then
+  pip install -q gdown
+  gdown $URL -O $OUTPUT
+fi
+
+OUTPUT=$HERE/data/edges2shoes_net_G_from_chainer.npz
+URL=https://drive.google.com/uc?id=1xAKLjtszp1d0AklVIPK_HqRjuYncDRuE
+if [ ! -e $OUTPUT ]; then
+  pip install -q gdown
+  gdown $URL -O $OUTPUT
 fi
