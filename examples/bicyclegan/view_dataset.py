6e857d62c5368214fb7553a98331bb34e346c149
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-01-02 23:04:12 +0900

    Update examples/bicyclegan/view_dataset.py

--- examples/bicyclegan/view_dataset.py
+++ examples/bicyclegan/view_dataset.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 
+import argparse
+
 import cv2
 import numpy as np
 
 from chainer_cyclegan.datasets import BerkeleyPix2PixDataset
 
 
-dataset = BerkeleyPix2PixDataset('edges2shoes', split='train')
-for i in range(len(dataset)):
-    img_A, img_B = dataset[i]
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-s', '--split', default='train',
+                        choices=['train', 'val'], help='dataset split')
+    args = parser.parse_args()
 
-    viz = np.hstack([img_A, img_B])
+    dataset = BerkeleyPix2PixDataset('edges2shoes', split=args.split)
+    for i in range(len(dataset)):
+        img_A, img_B = dataset[i]
 
-    cv2.imshow(__file__, viz[:, :, ::-1])
-    if cv2.waitKey(0) == ord('q'):
-        break
+        viz = np.hstack([img_A, img_B])
+
+        cv2.imshow(__file__, viz[:, :, ::-1])
+        if cv2.waitKey(0) == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+    main()
